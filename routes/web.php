af02ccc3eb1619127f15cbb95101477c55c46d39
Sotatek-HungPham3 <hung.pham3@sotatek.com>
2023-03-28 17:41:34 +0700

    new test

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,6 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get('/login', [\App\Http\Controllers\LoginController::class, 'ok']);
-
 Route::get('/', function () {
     return view('welcome');
 });
