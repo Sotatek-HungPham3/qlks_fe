a47ee88af3f9c9a675ef6c4e303efb0db1642b68
Sotatek-HungPham3 <hung.pham3@sotatek.com>
2023-03-28 16:43:15 +0700

    test ci

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+
+class LoginController extends Controller
+{
+    public function ok(): JsonResponse
+    {
+        return 1;
+    }
+}
